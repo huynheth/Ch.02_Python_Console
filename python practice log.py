913bc26340fd0b606ffdaa80c7bb3e0cf1da7814
huynheth <huynheth@urbandaleschools.com>
2022-09-09 08:59:35 -0500

    python practice sep 9

--- python practice log.py
+++ python practice log.py
@@ -9,4 +9,26 @@ a=2
 b=7
 print(a*b)
 print(b**a)
+print()
 print(b%2)
+
+a,b=(5,"ethan")
+print(a)
+print(b)
+print()
+x=8
+print(x)
+x*=2 #same as x=x*2
+print(x)
+bulletsShot=0
+bulletsShot+=3
+print(bulletsShot)
+print()
+a=("this is some text")
+b=3
+c=4.3
+d=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
