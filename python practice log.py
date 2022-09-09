3854aca0c8418b524c4d4453215d938d596e1935
huynheth <huynheth@urbandaleschools.com>
2022-09-09 08:41:08 -0500

    turtorial september 9

---
+++ python practice log.py
@@ -0,0 +1,12 @@
+# this is my learning about python
+# this is another line
+print("hello world")
+print(10.1//2) #floor division
+print(3**3) #exponents
+print(29%3) #remainders/modulus
+print(abs(-7)) #absolute value
+a=2
+b=7
+print(a*b)
+print(b**a)
+print(b%2)
