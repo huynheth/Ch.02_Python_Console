38ddaef04d6a23ef06bde4a0f18bfd5823ff43f7
huynheth <huynheth@urbandaleschools.com>
2022-09-09 09:19:27 -0500

    jedi training sep 9

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,23 +1,27 @@
 '''
-Sign your name:________________
+Sign your name:ethan
 
 1.) How do you enter a single line comment in a program? Give an example.
-
+using a hashtag
 
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
+'''
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+a=2
+b=5
 
+print(b/a)                 #=
+print(b//a)                #=
+print(b**a)                #=
+print(b%a)                 #=
+print(a+b)                 #=
+print(type(42))            #=
+print(type(42.0))          #=
+print(type("C3PO"))        #=
+print(type(True))          #=
 
+'''
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
 into the Python Console Window?
 
@@ -38,7 +42,7 @@ a
 
 
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
-
+'''
 x,y,z =(3,4,5)
 ave = x+y+z/3
 ave
