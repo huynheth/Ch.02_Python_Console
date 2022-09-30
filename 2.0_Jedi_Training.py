8c9d41c7cc0f7313ab43dfabdec879061e1c43ff
huynheth <huynheth@urbandaleschools.com>
2022-09-30 08:21:58 -0500

    number analysis doneeee

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,29 +1,29 @@
-'''
-Sign your name:ethan
+#
+# Sign your name:ethan
+#
+# 1.) How do you enter a single line comment in a program? Give an example.
+# using a hashtag
+# #like this
+#
+# 2.) Enter a=2 and b=5 in the Python Console window and then all of the following.
+# What are the outputs? If the output is an error record the error and try to determine what the error is!
+#
+# a=2
+# b=5
+#
+# print(b/a)              =2.5
+# print(b//a)             =2
+# print(b**a)             =25
+# print(b%a)              =1
+# print(a+b)              =7
+# print(type(42))         =int
+# print(type(42.0))       =float
+# print(type("C3PO"))     =string
+# print(type(True))       =bool
+# '''
+# #3.) What is the final output of (a) and type(a) if you enter the following 5 lines
+# #into the Python Console Window?
 
-1.) How do you enter a single line comment in a program? Give an example.
-using a hashtag
-#like this
-
-2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
-What are the outputs? If the output is an error record the error and try to determine what the error is!
-
-a=2
-b=5
-
-print(b/a)              =2.5
-print(b//a)             =2
-print(b**a)             =25
-print(b%a)              =1
-print(a+b)              =7
-print(type(42))         =int
-print(type(42.0))       =float
-print(type("C3PO"))     =string
-print(type(True))       =bool
-'''
-#3.) What is the final output of (a) and type(a) if you enter the following 5 lines
-#into the Python Console Window?
-'''
 
 a=2
 a*=10
@@ -32,27 +32,29 @@ a+=12
 a-=7
 a
 type(a)
-'''
-#4.) What is the mistake in the following code. Fix it!
 
-#forgot multiplication symbol
+# a = 15
 
+# #4.) What is the mistake in the following code. Fix it!
+#
+# #forgot multiplication symbol
+#
+#
+# '''
+# x,y = (4,5)
+# a=3*(x + y)
+# print(a)
+#
+#
+#
+# #5.) What is the mistake in the following code so it will calculate the average. Fix it!
+#
+# #needed to put "print" in front of "ave" and needed parentheses
+#
+#
+# x,y,z =(3,4,5)
+# ave = (x+y+z)/3
+# print(ave)
 
-'''
-x,y = (4,5)
-a=3*(x + y)
-print(a)
-
-
-
-#5.) What is the mistake in the following code so it will calculate the average. Fix it!
-
-#needed to put "print" in front of "ave" and needed parentheses 
-
-
-x,y,z =(3,4,5)
-ave = (x+y+z)/3
-print(ave)
-'''
 
 
